60da157347f988fc4efc0f713fb18c9a1e5d83a7
Terminatorjjjjj <r06943011@ntu.edu.tw>
2021-09-03 22:34:33 +0800

    update script for install

--- dotfiles.sh
+++ dotfiles.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
 
-ln -s $1/bash/bash_profile ~/.bash_profile
-ln -s $1/git/gitignore_global/ ~/.gitignore_global
-ln -s $1/tmux/tmux.conf ~/.tmux.conf
-ln -s $1/vim/ ~/.vim
-ln -s $1/vim/vimrc ~/.vimrc
+ln -s $1 ~/.dotfiles
+
+ln -s ~/.dotfiles/bash/bash_profile ~/.bash_profile
+ln -s ~/.dotfiles/git/gitignore_global/ ~/.gitignore_global
+ln -s ~/.dotfiles/tmux/tmux.conf ~/.tmux.conf
+ln -s ~/.dotfiles/vim/ ~/.vim
+ln -s ~/.dotfiles/vim/vimrc ~/.vimrc
+
+
