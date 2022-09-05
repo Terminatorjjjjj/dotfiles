c319bc2187be9d8b63f6856d68547db5357f5f41
Terminatorjjjjj <r06943011@ntu.edu.tw>
2022-09-05 20:01:06 +0800

    add lualine

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,4 +1,37 @@
 
+-- Packer {{{
+
+local packer_path = vim.fn.stdpath('config') .. '/site'
+vim.o.packpath = vim.o.packpath .. ',' .. packer_path
+
+require('packer').startup({function(use)
+    use 'wbthomason/packer.nvim'
+
+    use 'ervandew/supertab'
+    use 'tpope/vim-unimpaired'
+
+    use {
+        'nvim-lualine/lualine.nvim',
+        requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
+    use 'morhetz/gruvbox'
+    use 'arcticicestudio/nord-vim'
+    use 'joshdick/onedark.vim'
+    use 'sainnhe/everforest'
+    use 'sainnhe/gruvbox-material'
+end,
+config = {
+    display = {
+        open_fn = function()
+            return require('packer.util').float({ border = 'rounded' })
+        end
+    },
+    package_root = vim.fn.stdpath('config') .. '/site/pack'
+}})
+
+-- }}}
+
 -- Setting: {{{
 
 vim.opt.expandtab = true
@@ -13,6 +46,7 @@ vim.opt.splitright = true
 vim.opt.splitbelow = true
 vim.opt.wildignore = {'*.docx' ,'*.jpg' ,'*.png' ,'*.gif' ,'*.pdf' ,'*.pyc' ,'*.exe' ,'*.flv' ,'*.img' ,'*.xlsx' ,'*.zip' ,'*.so' ,'*.swp' ,'*/tmp/*' ,'*/.git/*'}
 vim.opt.fillchars = { vert = ' ' }
+vim.opt.termguicolors = true
 
 -- Make shell not source startup scripts when running shell command in vim
 vim.opt.shellcmdflag = '-f -c'
@@ -123,36 +157,81 @@ vim.api.nvim_create_autocmd(
 )
 
 -- }}}
-
--- Packer {{{
-
-local packer_path = vim.fn.stdpath('config') .. '/site'
-vim.o.packpath = vim.o.packpath .. ',' .. packer_path
-
-require('packer').startup({function(use)
-    use 'wbthomason/packer.nvim'
-
-    use 'morhetz/gruvbox'
-    use 'arcticicestudio/nord-vim'
-    use 'joshdick/onedark.vim'
-end,
-config = {
-    display = {
-        open_fn = function()
-            return require('packer.util').float({ border = 'rounded' })
-        end
-    },
-    package_root = vim.fn.stdpath('config') .. '/site/pack'
-}})
-
--- }}}
-
 -- Colorscheme {{{
 
-vim.cmd('colorscheme nord')
+vim.g.everforest_background = 'hard'
+vim.g.everforest_better_performance = 1
+
+vim.cmd('colorscheme everforest')
 vim.cmd('hi CursorLine ctermbg=16')
 vim.cmd('hi CursorLineNr ctermbg=16')
 vim.cmd('hi! link Folded Comment')
 
 -- }}}
+
+-- Lualine {{{
+
+require('lualine').setup {
+  options = {
+    icons_enabled = true,
+    theme = 'auto',
+    component_separators = { left = '', right = ''},
+    section_separators = { left = '', right = ''},
+    disabled_filetypes = {
+      statusline = {},
+      winbar = {},
+    },
+    ignore_focus = {},
+    always_divide_middle = true,
+    globalstatus = false,
+    refresh = {
+      statusline = 1000,
+      tabline = 1000,
+      winbar = 1000,
+    }
+  },
+  sections = {
+    lualine_a = {'mode'},
+    lualine_b = {'branch'},
+    lualine_c = {
+        {
+            'filename',
+            path = 1, -- Relative path
+            symbols = {
+                modified = ' +',
+                readonly = ' ‼',
+                unnamed = ' ?',
+            }
+        }
+    },
+    lualine_x = {'diagnostics'},
+    lualine_y = {'diff'},
+    lualine_z = {'%3c'}
+  },
+  inactive_sections = {
+    lualine_a = {},
+    lualine_b = {},
+    lualine_c = {
+        {
+            'filename',
+            path = 1, -- Relative path
+            symbols = {
+                modified = ' +',
+                readonly = ' ‼',
+                unnamed = ' ?',
+            }
+        }
+    },
+    lualine_x = {'%3c'},
+    lualine_y = {},
+    lualine_z = {}
+  },
+  tabline = {},
+  winbar = {},
+  inactive_winbar = {},
+  extensions = { 'quickfix' }
+}
+
+-- }}}
+
 
